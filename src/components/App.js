8c3027ba0f801f82ebb270e27564273cc0c6d0ff
Antonio <antonio18casta@aol.com>
2023-02-21 16:47:14 -0500

    commit tree d79def4ec524324b01fbd23afef041d2aaa7132e

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React,  {useEffect, useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
+const API = "http://localhost:3001/pizzas"
+
 function App() {
+
+  const [pizzas, setPizzas] = useState([]);
+  const [selectedPizza, setSelectedPizza] = useState({});
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(setPizzas)
+  }, [])
+
+  const selectPizza = (pizzaObj) => {
+    setSelectedPizza(pizzaObj)
+  }
+
+  const handlePizzaChange = (pizzaObj) => {
+
+    fetch(`http://localhost:3001/pizzas/${pizzaObj.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pizzaObj)
+    })
+    .then(res => res.json())
+    .then(_ => {
+      const updatedPizzaList = [...pizzas].map(pizza => {
+        if(pizza.id === pizzaObj.id){
+          return pizzaObj
+        } else {
+            return pizza
+        }
+      })
+      setPizzas(updatedPizzaList)
+    })
+  }
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm selectedPizza={selectedPizza} handlePizzaChange={handlePizzaChange} />
+      <PizzaList pizzas={pizzas} selectPizza={selectPizza} />
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-function PizzaForm() {
+function PizzaForm({ selectedPizza, handlePizzaChange }) {
+
+  const [id, setPizzaId] = useState(0);
+  const [topping, setTopping] = useState("");
+  const [size, setSize] = useState("");
+  const [vegetarian, setVegetarian] = useState(false);
+
+  useEffect(() => {
+    setPizzaId(selectedPizza.id)
+    setTopping(selectedPizza.topping)
+    setSize(selectedPizza.size)
+    setVegetarian(selectedPizza.vegetarian)
+  }, [selectedPizza])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const updatedPizza = {
+      id: id,
+      topping: topping,
+      size: size,
+      vegetarian: vegetarian
+    }
+    handlePizzaChange(updatedPizza)
+  }
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,15 +35,19 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            onChange={(e) => setTopping(e.target.value)}
+            value={topping}
           />
         </div>
+
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value={size} onChange={(e) => setSize(e.target.value)}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
           </select>
         </div>
+
         <div className="col">
           <div className="form-check">
             <input
@@ -26,18 +55,24 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              onChange={() => setVegetarian(true)}
+              checked={vegetarian}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
+
           <div className="form-check">
             <input
               className="form-check-input"
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              onChange={() => setVegetarian(false)}
+              checked={!vegetarian}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
+
         </div>
         <div className="col">
           <button type="submit" className="btn btn-success">
